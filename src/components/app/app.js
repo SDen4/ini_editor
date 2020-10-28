8fb1442885302886098d35f2a4b130fabea6a811
SDen4 <asw325@yandex.ru>
2020-10-28 23:23:05 +0500

    app markup

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,24 @@
 import React, {Component} from 'react';
+import Editor from '../editor/editor';
 
 
 class App extends Component {
-    state = {
-        reverted: false
-    }
     render() {
         return (
-            <article className="app">
-                <h1>Title of the APP ini-editor</h1>
+            <article className='app'>
+                <h1 className='app__title'>ini-editor</h1>
+                <section className='app__container'>
+                    <Editor />
+                    <div className='app__buttons'>
+                        <div className='app__buttons_wrapper'>
+                            <button className='button button__mode' type='button'>Text mode</button>
+                            <button className='button button__mode' type='button'>Graph mode</button>
+                        </div>
+                        <div className='app__buttons_wrapper'>
+                            <button className='button button__mode' type='button'>Download file</button>
+                        </div>
+                    </div>
+                </section>
             </article>
         );
     }
---
+++ src/components/editor/editor.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class Editor extends Component {
+    render() {
+        return (
+            <article className='editor'>
+                <h1 className='editor__title'>editor</h1>
+            </article>
+        );
+    }
+};
+
+export default Editor;
