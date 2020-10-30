fbe0245b3a522877376897d5428abc5c92b07cb0
SDen4 <asw325@yandex.ru>
2020-10-30 20:14:39 +0500

    downloading file

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,12 +3,15 @@ import Editor from '../editor/editor';
 
 
 class App extends Component {
+    state = {
+        object: {}
+    }
     render() {
         return (
             <article className='app'>
                 <h1 className='app__title'>ini-editor</h1>
                 <section className='app__container'>
-                    <Editor />
+                    <Editor objectText={this.state.object} />
                     <div className='app__buttons'>
                         <div className='app__buttons_wrapper'>
                             <button className='button button__mode' type='button'>Text mode</button>
@@ -19,6 +22,7 @@ class App extends Component {
                                 id='app__input'
                                 className='button button__mode'
                                 type='file'
+                                onChange={(e)=>this.handleFileLoad(e)}
                             ></input>
                             <div className='button button__mode button__mode_input'>Download file</div>
                         </label>
@@ -26,6 +30,54 @@ class App extends Component {
                 </section>
             </article>
         );
+    }
+    handleFileLoad(e) {
+        e.preventDefault();
+
+        let file = e.target.files[0];
+        let reader = new FileReader();
+        reader.readAsText(file);
+
+        reader.onloadend = () => {
+            let obj = this.parseINIString(reader.result);
+            console.log(obj);
+            this.setState({
+              object: obj
+            })
+        };
+
+        // let res = this.parseINIString(this.state.file);
+        // localStorage.setItem('file', res);
+    }
+    //parser ini to object
+    parseINIString(data){
+        let regex = {
+            section: /^\s*\[\s*([^\]]*)\s*\]\s*$/,
+            param: /^\s*([^=]+?)\s*=\s*(.*?)\s*$/,
+            comment: /^\s*;.*$/
+        };
+        let value = {};
+        let lines = data.split(/[\r\n]+/);
+        let section = null;
+        lines.forEach(function(line){
+            if(regex.comment.test(line)){
+                return;
+            }else if(regex.param.test(line)){
+                let match = line.match(regex.param);
+                if(section){
+                    value[section][match[1]] = match[2];
+                }else{
+                    value[match[1]] = match[2];
+                }
+            }else if(regex.section.test(line)){
+                let match = line.match(regex.section);
+                value[match[1]] = {};
+                section = match[1];
+            }else if(line.length == 0 && section){
+                section = null;
+            };
+        });
+        return value;
     }
 };
 
--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -3,9 +3,12 @@ import React, {Component} from 'react';
 
 class Editor extends Component {
     render() {
+        // const text = this.props.objectText ? this.props.objectText : '';
+        console.log(this.props.objectText);
         return (
             <article className='editor'>
                 <h1 className='editor__title'>editor</h1>
+                {/* <div className='editor__content'>{text}</div> */}
             </article>
         );
     }
