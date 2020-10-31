c77d1267c79d411e986cc3a1c6953e217b066013
SDen4 <asw325@yandex.ru>
2020-10-31 23:09:28 +0500

    download modified ini-file from text mode is ready

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -3,7 +3,8 @@ import React, {Component} from 'react';
 
 class Editor extends Component {
     state = {
-        iniText: ''
+        iniText: '',
+        buttonSaveActive: false,
     }
     render() {
         const listInfo = this.props.info.map( (key, index) => { 
@@ -15,14 +16,19 @@ class Editor extends Component {
 
         const editorGraph = <ul className='editor__wrapper'>{listKeys}{listInfo}</ul>;
 
-        const editorText = <label className='editor__wrapper'>
-            <textarea
-                className='editor__textMode'
-                defaultValue={this.props.str}
-                onChange={this.handleChange}
-            ></textarea>
-            <button className='button button__mode button__save' type='button'>Save</button>
-        </label>;
+        const editorText = 
+            <form className='editor__wrapper' onSubmit={this.saveTextMode}>
+                <textarea
+                    className='editor__textMode'
+                    defaultValue={this.props.str}
+                    onChange={this.handleChange}
+                ></textarea>
+                <button
+                    // className='button button__mode button__save'
+                    className={`${this.state.buttonSaveActive && 'button__mode'} ${'button button__save'}`}
+                    type='submit'
+                >Save</button>
+            </form>;
 
         const listKeysGraphMode = this.props.graphMode && editorGraph;
         const listKeysTextMode = !this.props.graphMode && editorText;
@@ -32,20 +38,40 @@ class Editor extends Component {
                 <h1 className='editor__title'>{this.props.graphMode ? 'Graph mode' : 'Text mode'}</h1>
                 {listKeysGraphMode}
                 {listKeysTextMode}
-                {/* <ul className='editor__list'> */}
-                    {/* {listKeys} */}
-                    {/* <button className='test' onClick={this.counting}>Test</button> */}
-                {/* </ul> */}
-                {/* <ul className='editor__list'> */}
-                    {/* {listInfo} */}
-                {/* </ul> */}
             </article>
         );
     }
     handleChange = (e) => {
         this.setState({
-            iniText: e.target.value
+            iniText: e.target.value,
+            buttonSaveActive: true
         })
+    }
+    saveTextMode = (e) => {
+        e.preventDefault();
+
+        //protect saving witout changes & empty files
+        if(!this.state.buttonSaveActive) return;
+
+        let data = this.state.iniText;
+        let file = new Blob([data], {type: 'ini'});
+
+        //IE10
+        if (window.navigator.msSaveOrOpenBlob)
+            window.navigator.msSaveOrOpenBlob(file, 'new_ini_file.ini');
+        //others
+        else {
+            let a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+            a.href = url;
+            a.download = 'new_ini_file.ini';
+            document.body.appendChild(a);
+            a.click();
+            setTimeout(function() {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(url);
+            }, 0);
+        }
     }
 };
 
