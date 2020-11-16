9d1dee858dccf2ca19213a907464649edb72e65b
SDen4 <asw325@yandex.ru>
2020-11-16 22:11:24 +0500

    dark mode button markup

--- src/components/aside/aside.js
+++ src/components/aside/aside.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 
 class Aside extends Component {
     state = {
-        graphMode: true,
+        graphMode: true
     }
     componentDidUpdate(prevProps) {
         if (this.props.graphMode === prevProps.graphMode) return;
@@ -14,21 +14,26 @@ class Aside extends Component {
     render() {
         return (
             <aside className='aside__buttons'>
-                <button
-                    className='button button__mode'
-                    type='button'
-                    onClick={this.changeMode}
-                >{this.state.graphMode ? 'Text mode' : 'Graph mode'}</button>
-                <label className='aside__buttons_wrapper' htmlFor='app__input'>
-                    <input
-                        id='app__input'
+                <div className='aside__buttons_block'>
+                    <button
                         className='button button__mode'
-                        type='file'
-                        accept=".ini"
-                        onChange={(e)=>this.handleFileLoad(e)}
-                    ></input>
-                    <div className='button button__mode button__mode_input'>Download ini&#8209;file</div>
-                </label>
+                        type='button'
+                        onClick={this.changeMode}
+                    >{this.state.graphMode ? 'Text mode' : 'Graph mode'}</button>
+                    <label className='aside__buttons_wrapper' htmlFor='app__input'>
+                        <input
+                            id='app__input'
+                            className='button button__mode'
+                            type='file'
+                            accept=".ini"
+                            onChange={(e)=>this.handleFileLoad(e)}
+                        ></input>
+                        <div className='button button__mode button__mode_input'>Download ini&#8209;file</div>
+                    </label>
+                </div>
+                <div className='aside__buttons_block'>
+                    <button className='button button__mode'>Dark</button>
+                </div>
             </aside>
         );
     }
